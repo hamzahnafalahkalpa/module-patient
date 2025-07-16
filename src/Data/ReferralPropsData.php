23240076575b3db10e8b5c687336f0fe09508f6d
Hamzah <hamzahnafalah@gmail.com>
2025-07-16 22:52:52 +0700

    upadte modules

--- src/Data/ReferralPropsData.php
+++ src/Data/ReferralPropsData.php
@@ -4,11 +4,11 @@ namespace Hanafalah\ModulePatient\Data;
 
 use Hanafalah\LaravelSupport\Supports\Data;
 use Hanafalah\ModulePatient\Contracts\Data\ExternalReferralData;
-use Hanafalah\ModulePatient\Contracts\Data\ReferralpropsData as DataReferralpropsData;
+use Hanafalah\ModulePatient\Contracts\Data\ReferralPropsData as DataReferralPropsData;
 use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\MapName;
 
-class ReferralpropsData extends Data implements DataReferralPropsData{
+class ReferralPropsData extends Data implements DataReferralPropsData{
     #[MapInputName('external_referral')]
     #[MapName('external_referral')]
     public ?ExternalReferralData $external_referral = null;
